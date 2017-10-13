f47d1a42f16ad9f81dd4efb8de19adda7b27e51b
Ryan Ramphal <rramphal94@gmail.com>
2017-10-13 11:33:03 -0400

    'Finished' view member page (from ideator perspective).

--- js/script.js
+++ js/script.js
@@ -3,9 +3,24 @@ $(function(){
         $("#contactModal").modal('show');
     });
 
-    //$("#input-1").rating();
-
     $('#rateButton').on('click', function(){
         $('#rateModal').modal('show');
     });
+
+    $("#rateYo").rateYo({
+        onSet: function (rating, rateYoInstance) {
+            rating = Math.ceil(rating);
+            $('#rating_input').val(rating);
+
+         }
+    });
+
+    $('#reviewSubmit').on('click', function(){
+        $.post("/review", {
+            reviewee = $('#profileID').val(),
+            reviewer = $('#userID').val(),
+            reviewText = $('#reviewBody').val()
+        });
+    });
+
 });
