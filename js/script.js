a0f9fe6aa74c7a15f24100b4c5d674d454aec1da
Ryan Ramphal <rramphal94@gmail.com>
2017-10-13 00:35:12 -0400

    Snapshot of when Ryan & Anna left.

--- js/script.js
+++ js/script.js
@@ -2,4 +2,10 @@ $(function(){
     $("#contactButton").on('click', function(){
         $("#contactModal").modal('show');
     });
+
+    //$("#input-1").rating();
+
+    $('#rateButton').on('click', function(){
+        $('#rateModal').modal('show');
+    });
 });
