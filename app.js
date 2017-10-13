461e1b8c1e203001c8f2f590ec210f6bb2544949
Ryan Ramphal <rramphal94@gmail.com>
2017-10-13 15:20:15 -0400

    Updated server files.

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var path = require("path");
 var firebase = require('firebase');
+var bodyparser = require('body-parser');
+
 app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
 
 var config = {
     apiKey: "AIzaSyAYNtFQO6DHnLNumEDuNCjqGYBXXjlfY7s",
@@ -22,13 +26,24 @@ firebaseRef.on('value', (snapshot) =>{
 
 });
 
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: false}));
+app.use(express.static(__dirname + "/public"));
+
 app.use('/member/:id', function(req,res,next){
     //create objects to pass into the view
-    
+    var profile = {};
+    var user = {};
+    firebaseRef.on('value', (snapshot) => {
+        profile = snapshot[req.params.id];
+        user = snapshot[0];
+    });
 
-    res.render('member', );
+    res.render('member', {profile: profile, user: user});
 });
 
 console.log(profiles);
 
-app.listen(8080);
+app.listen(8080, function(){
+    console.log("App listening on port 8080.");
+});
