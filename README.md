099ceb31f5dee4fde1914b49d3d893742f2b86b5
Ryan Ramphal <rramphal94@gmail.com>
2017-11-04 00:04:44 -0400

    Minimal Express / Handlebars backend for PinLab site.

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
-# purehouse
-purehouse project
+# PUREHOUSE LAB Team (PC069)
+
+# PinLab
 
 Team Members:
+=============
 An Le - aql6
+Anna Guidi
+Barron Zuo
+Ryan Ramphal - rdr233
+Will Davis
 
-[![Waffle.io - Issues in progress](https://badge.waffle.io/rramphal100/purehouse.svg?label=in%20progress&title=In%20Progress)](http://waffle.io/zxcynk/purehouse)
+[![Waffle.io - Issues in progress](https://badge.waffle.io/rramphal100/purehouse.svg?label=in%20progress&title=In%20Progress)](http://waffle.io/rramphal100/purehouse)
+
+## Usage
+1. Clone this repository.
+2. Run `npm install`.
+3. Obtain the correct environment file from the developers and save it in the project directory.
+4. Run `nodemon app` to start the server. Take note of the port number that is displayed on successful startup of the daemon.
+5. Go to `http://localhost:PORT` where PORT = the port number shown in the console output from the previous step.
--- app.js
+++ app.js
@@ -1,34 +1,53 @@
 var app = require('express')();
 var path = require("path");
 var firebase = require('firebase');
+var dotenv = require('dotenv-safe').load();
+var bodyParser = require('body-parser');
+
 app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 var config = {
-    apiKey: "AIzaSyAYNtFQO6DHnLNumEDuNCjqGYBXXjlfY7s",
-    authDomain: "idearank-47142.firebaseapp.com",
-    databaseURL: "https://idearank-47142.firebaseio.com",
-    projectId: "idearank-47142",
-    storageBucket: "idearank-47142.appspot.com",
-    messagingSenderId: "441163562896"
+    apiKey: process.env.APIKEY,
+    authDomain: process.env.AUTHDOMAIN,
+    databaseURL: process.env.DATABASEURL,
+    projectId: process.env.PROJECTID,
+    storageBucket: process.env.STORAGEBUCKET,
+    messagingSenderId: process.env.MESSAGINGSENDERID
 };
 
 var fire = firebase.initializeApp(config);
 
 var profiles = [];
-var firebaseRef = fire.database().ref('/profile/');
-firebaseRef.on('value', (snapshot) =>{
+var firebaseProfileRef = fire.database().ref('/profile/');
+firebaseProfileRef.on('value', (snapshot) =>{
   profiles = snapshot.val();
-  console.log(profiles);
-
 });
 
-app.use('/member/:id', function(req,res,next){
-    //create objects to pass into the view
-    
-
-    res.render('member', );
+//sample routes to show how routes are created and rendered
+//note the http method is specified here (get, post, etc)
+//req = request object, res = response object, next = error catcher / callback function
+app.get('/', function(req,res,next){
+    res.render('home', {pageTitle: "PinLab"});
 });
 
-console.log(profiles);
+app.get('/profiles', function(req, res, next){
+    res.render('profiles', {pageTitle: "Profiles", profiles: profiles});
+});
 
-app.listen(8080);
+//demo how request parameters are used
+app.get('/params/:text', function(req, res, next){
+    res.send(req.params.text);
+});
+
+hostport = 8080;
+if (process.env.NODE_ENV === 'PRODUCTION'){
+    hostport = 80;
+}
+
+app.listen(hostport, function(){
+    console.log("PinLab is running on port " + hostport.toString());
+});
