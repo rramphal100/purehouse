0129f8185c6b05c398722f2d74438484188ff812
An Le <aql6@cornell.edu>
2017-11-14 20:09:24 -0500

    Add side nav

--- public/js/init.js
+++ public/js/init.js
@@ -1,7 +1,12 @@
 (function($){
   $(function(){
+    // Initialize collapse button
+    $(".button-collapse").sideNav();
+    // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+    //$('.collapsible').collapsible();
 
-    $('.button-collapse').sideNav();
-
+    $(".side-nav-toggle").click(function(){
+      $('.button-collapse').sideNav('show');
+    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
